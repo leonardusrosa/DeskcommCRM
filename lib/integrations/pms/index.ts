912a2d5abbefab36354ddf238a646e979abe63eb
leonardusrosa <leo_box@outlook.com>
2026-09-17 19:20:50 -0300

    chore(pms): export redaction helper

--- lib/integrations/pms/index.ts
+++ lib/integrations/pms/index.ts
@@ -6,6 +6,7 @@ export * from "./capabilities";
 export * from "./credentials";
 export * from "./mapping";
 export * from "./entity-projector";
+export * from "./redaction";
 export * from "./connection-repository";
 export * from "./newsoft-connector";
 export * from "./sync-engine";
